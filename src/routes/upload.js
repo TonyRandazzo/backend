20be03023fe7ed0ca8e5fd6ecc4e3abccb215ced
TonyRandazzo <randazzotony1@gmail.com>
2026-06-14 16:01:42 +0200

    aggiornamento case sensitive

--- src/routes/upload.js
+++ src/routes/upload.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const multer = require('multer');
-const supabase = require('../config/supabaseStorage');
+const supabase = require('../config/SupabaseStorage');
 const auth = require('../middleware/auth');
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
